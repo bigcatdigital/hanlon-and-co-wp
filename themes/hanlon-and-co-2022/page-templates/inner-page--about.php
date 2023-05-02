a65362a40fe38d83f36cc6bbcda04916617c0413
bigcatdigital <mmason2@hotmail.com>
2023-05-02 15:25:42 +0100

    Edits to about page for profiles sort order

--- themes/hanlon-and-co-2022/page-templates/inner-page--about.php
+++ themes/hanlon-and-co-2022/page-templates/inner-page--about.php
@@ -81,21 +81,27 @@
 					</div><!-- // .bc-content-component -->
 				<?php } ?> 
 				<div class="bc-content-component bc-profiles">
-					<div class="bc-grid">
+					<div class="bc-grid bc-match-height-wrap">
 						<?php 
 							$profile_posts = new WP_Query(array(
-								'post_type' => 'staffprofile'
+								'post_type' => 'staffprofile',
+								'meta_key' => 'profile-sort-order',
+								'orderby' => 'meta_value',
+								'order' => 'ASC'
 							));	
 							if ($profile_posts->have_posts()) {
 								while ($profile_posts->have_posts()) {
 									$profile_posts->the_post(); 
-									$profile_image = get_field('profile-photo');
-									?>
-						<article class="bc-card bc-profile-card">
+									$profile_image = get_field('profile-photo'); ?>
+						<article class="bc-card bc-profile-card bc-match-height">
 							<div class="bc-card__body">
+								<?php if ($profile_image['url'] && strcmp($profile_image['url'], '') !== 0) { ?>
 								<img class="bc-card__media" src="<?php echo $profile_image['url'] ?>" alt="<?php echo $profile_image['alt'] ?>">
-							<h2 class="bc-card__heading"><?php the_title() ?></h2>
-							<p class="bc-profile__title"><?php the_field('profile-title') ?></p>
+								<?php } //end if profile_image[url] ?>
+								<h2 class="bc-card__heading"><?php the_title() ?></h2>
+								<?php if (get_field('profile-title') && strcmp(get_field('profile-title'), '') !== 0) { ?>
+								<p class="bc-profile__title"><?php the_field('profile-title') ?></p>
+								<?php } // end if profile-title ?>
 							</div>
 							<div class="bc-card__link">
 								<p><a href="<?php the_permalink(); ?>">Read more here</a></p>
